a540968d19d675082f2fa0dabce0baee76af5ee2
senyo isaac <isaacsenyo6@gmail.com>
2025-05-25 10:35:19 +0000

    Initial commit: Add basic_oerations.py

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,10 @@
+number1 = 10
+number2 = 5
+
+sum_result = number1 + number2
+difference = number1 - number2
+product = number1 * number2
+
+print(f"sum of {number1} and {number2} is {sum_result}")
+print(f"difference of {number1} and {number2} is {difference}")
+print(f"product of {number1} and {number2} is {product}")
