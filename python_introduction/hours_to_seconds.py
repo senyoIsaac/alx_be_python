b8fd504bd2b5e2410e75a69b4be4dede431d6312
senyo isaac <isaacsenyo6@gmail.com>
2025-05-25 11:15:07 +0000

    future_age_calculation

---
+++ python_introduction/hours_to_seconds.py
@@ -0,0 +1,8 @@
+# Define the number of hours to convert
+hours = 2  # Number of hours
+
+# Calculate the equivalent seconds (1 hour = 3600 seconds)
+seconds = hours * 3600
+
+# Print the conversion result
+print(f"{hours} hour(s) is {seconds} seconds")
