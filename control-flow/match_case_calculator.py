696b1df58fcec77bc6715b3df5a4fdf522742dcc
senyo isaac <isaacsenyo6@gmail.com>
2025-05-31 23:06:29 +0000

    First commit

---
+++ control-flow/match_case_calculator.py
@@ -0,0 +1,7 @@
+num1 = input("Enter the first number:")
+num2 = input("Enter the second number:")
+operation = input("Enter the type of operation")
+
+match operation:
+    case addition:
+        num1 + num2
