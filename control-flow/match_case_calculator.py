e28ff65867793c9ab4caec51df786a7489aedab0
senyo isaac <isaacsenyo6@gmail.com>
2025-05-31 23:27:20 +0000

    New commit, exended the match cases

--- control-flow/match_case_calculator.py
+++ control-flow/match_case_calculator.py
@@ -1,7 +1,26 @@
 num1 = input("Enter the first number:")
 num2 = input("Enter the second number:")
-operation = input("Enter the type of operation")
+operation = input("Choose the operation (+, -, *, /):")
 
 match operation:
-    case addition:
-        num1 + num2
+    case "+":
+        result = num1 + num2
+        print(f"The result is {result}")
+
+    case "-":
+        result = num1 - num2
+        print(f"The result is {result}")
+
+    case "*":
+        result = num1 * num2
+        print(f"The result is {result}")
+
+    case "/":
+        if num2 == 0:
+            print("Cannot divide by zero")
+        else:
+            result = num1 / num2
+            print(f"The result is {result}")
+        
+    case _:
+        print("Invalid operation selected. Please choose from +, -, *, /")
