2becb19e98e0364d3d433b7cdabe33f66017b0ff
senyo isaac <isaacsenyo6@gmail.com>
2025-06-08 21:14:00 +0000

    changes

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -13,12 +13,12 @@ def perform_operation(num1, num2, operation):
             #print(f"The result is {result}")
 
         case "divide":
-            if num2 == 0:
-                print("Cannot divide by zero")
-            else:
+            if num2 != 0:
                 result = num1 / num2
-                #print(f"The result is {result}")
-            
+                
+            elif num2 == 0:
+                print("Cannot divide by zero")
+
         case _:
             print("Invalid operation selected. Please choose from +, -, *, /")
 
