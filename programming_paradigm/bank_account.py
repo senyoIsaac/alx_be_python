507e7ffa22d9a9e9faf2e6cac283fce4669e590c
senyo isaac <isaacsenyo6@gmail.com>
2025-06-15 21:59:48 +0000

    .....

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -13,21 +13,4 @@ class BankAccount:
         if amount <= self.balance:
             self.balance -= amount
             print(f"Withdrew:${amount}")
-        else: 
-            print("Insufficient funds.")
-"""
-# Using the class
-account = BankAccount("Alice", 1000)
-
-# Try to access private attribute directly (won't work as intended)
-# print(account.__balance)  # ❌ AttributeError
-
-# Can't access __balance directly
-print(account.get_balance())  # ✅ 1000
-
-
-account.deposit(500)
-print(account.get_balance())  # ✅ 1500
-
-account.withdraw(2000)  # ❌ Insufficient balance
-"""
+    
